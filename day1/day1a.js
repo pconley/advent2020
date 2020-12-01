27a6ea148148e1ec6c6f95020f1ceec6d996c8eb
Patrick Conley <pat@Patricks-MacBook-Pro.local>
2020-12-01 15:02:11 -0500

    update

--- day1/day1a.js
+++ day1/day1a.js
@@ -1,4 +1,3 @@
-
 const _ = require('lodash');
 const fs = require('fs');
 const assert = require('assert');
@@ -10,54 +9,75 @@ const { sum } = require('lodash');
 const prod = arr => arr.reduce((a,b) => a * b, 1);
 
 var logger = log4js.getLogger();
-logger.level = 'debug'; // trace; debug; info, warn; error; fatal
+logger.level = 'debug'; // trace; debug; info; warn; error; fatal
 
 const [node, pgm, filename] = process.argv;
 
 console.log('Day One'.cyan);
 console.log("filename =", filename.bold);
 
-// function calc(mass){
-//   // take its mass, divide by three, round down, and subtract 2
-//   return Math.floor(mass/3.0)-2
-// }
-
-// assert(calc(12),2)
-// assert(calc(14),2)
-// assert(calc(1969),654)
-// assert(calc(100756),33585)
-
-
 fs.readFile(filename, 'utf8', function(err, contents) {
+
+  let solution2 = 0;
+  let solution3 = 0;
+
   // console.log(">>",contents);
   const lines = contents.split("\n");
   const values = lines.map( x => parseInt(x) );
 
+  // Combinatorics code is simpler
+
   // const pairs = Combinatorics.bigCombination(values, 2);
   // while(a = pairs.next()) {
-  //   if( sum(a) === 2020 ) console.log("found".red, a, prod(a));
+  //   if( sum(a) === 2020 ) console.log("found pair".red, a, prod(a));
   // }
 
   // const triples = Combinatorics.bigCombination(values, 3);
   // while(a = triples.next()) {
-  //   if( sum(a) === 2020 ) console.log("found".red,a, prod(a));
+  //   if( sum(a) === 2020 ) console.log("found triple".red, a, prod(a));
   // }
 
-  // this appears to be 10x faster
+  // But, this is 10x faster
 
-  for (const v1 of values) {
-    for (const v2 of values) {
-      if( v1 + v2 === 2020 ) {
-        console.log("pair", v1, v2, v1+v2, v1*v2);
-      }
-      for (const v3 of values) {
-        if( v1 + v2 + v3 === 2020 ) {
-          console.log("triple".red, v1, v2, v3, v1+v2+v3, v1*v2*v3);
+  // if we only wanted the FIRST solution
+  let found2 = false;
+  let found3 = false;
+
+  let count2 = 0;
+  let count3 = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    const v1 = values[i];
+    for (let j = i+1; j < values.length; j++) {
+      const v2 = values[j];
+      const sum2 = v1 + v2;
+      count2 += 1;
+      if( sum2 === 2020 ) {
+        found2 = true;
+        solution2 = v1 * v2;
+        console.log("pair".red, i, j, v1, v2, v1+v2, solution2);
+      } else if( sum2 > 2020 ) {
+        // skip the third values; can't be a solution
+      } else /* less than 2020 */ {
+        for (let k = j+1; k < values.length; k++) {
+          count3 += 1;
+          const v3 = values[k];
+          if( sum2 + v3 === 2020 ) {
+            found3 = true;
+            solution3 = v1 * v2 * v3;
+            console.log("triple".red, i, j, k, v1, v2, v3, v1+v2+v3, solution3);
+          }
+          if (found2 && found3) break;
         }
       }
+      if (found2 && found3) break;
     }
+    if (found2 && found3) break;
   }
-});
+  console.log("#values = ", values.length);
+  console.log("op2 count = ", count2);
+  console.log("op3 count = ", count3);
 
-// 539851
-// 212481360
+  assert(solution2, 539851)
+  assert(solution3, 212481360)
+});
